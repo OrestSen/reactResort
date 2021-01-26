62c2a24d01371edd7fba1fbfc2fc07ec515ca058
orest <oresttrue@gmail.com>
2021-01-26 12:04:38 +0200

    create components

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+import loadingGif from "../images/gif/loading-arrow.gif";
+
+
+export default function loading() {
+return (
+    
+<img src={loadingGif} alt="" />)
+}
---
+++ src/components/RoomFilter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title from '../components/Title';
+
+export default function RoomsFilter() {
+    const context = useContext(RoomContext);
+    console.log(context);
+    return <div>  hello from rooms filter</div>
+
+}
